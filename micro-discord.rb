3242270d776309f51dca7f68394b8264772a21f5
soukouki <soukouki0@yahoo.co.jp>
2017-10-18 22:56:44 +0900

    タイトルを設定するように

--- micro-discord.rb
+++ micro-discord.rb
@@ -11,6 +11,11 @@ print "\n\n"
 require "sinatra"
 require "discordrb"
 
+def body_part title, body_html
+	"<!DOCTYPE html><html><head><meta charset=\"UTF-8\"><title>#{title}</title></head><body>"+
+	body_html+"</body></html>"
+end
+
 # param:hash {"id":value}
 def create_select_html to_uri, hash
 	hash.map{|key, value|'<p><a href="'+to_uri+key.to_s+'">'+value+'</a></p>'}.join("")
@@ -29,23 +34,25 @@ get "/" do
 end
 
 get "/servers/" do
-	"<h1>micro discord</h1>"+
-	create_select_html("/server/?serverid=", bot.servers.map{|s|[s[0], s[1].name]}.to_h)
+	body_part("servers",
+		"<h1>micro discord</h1>"+
+		create_select_html("/server/?serverid=", bot.servers.map{|s|[s[0], s[1].name]}.to_h))
 end
 
 get "/server/" do
 	id = request.params["serverid"].to_i
 	server = bot.server(id)
-	"<h1><a href=\"/servers/\">servers</a> / #{server.name}</h1>"+
-	create_select_html(
-		"/channel/?channelid=",
-		server.channels.select{|c|c.type==0}
-			.map{|c|[c.id, c.name+((c.topic && !c.topic.empty?)? "<div style=\"margin-left: 3em;margin-top: -1em\">#{c.topic}</div>" : "")]}.to_h)
+	body_part(server.name,
+		"<h1><a href=\"/servers/\">servers</a> &gt; #{server.name}</h1>"+
+			create_select_html("/channel/?channelid=",
+				server.channels.select{|c|c.type==0}
+					.map{|c|[c.id, c.name+((c.topic && !c.topic.empty?)? "<div style=\"margin-left: 3em;margin-top: -1em\">#{c.topic}</div>" : "")]}.to_h))
 end
 
 get "/channel/" do
 	id = request.params["channelid"].to_i
 	channel = bot.channel(id)
+	server = channel.server
 	timeline = channel
 		.history(20)
 		.map{|msg|
@@ -54,11 +61,12 @@ get "/channel/" do
 			text = msg.text.gsub("\n"){"<br>"}.gsub(/\*\*(.*?)\*\*/){"<b>"+$1+"</b>"}
 			"<p>"+data+" : "+name+" : <span style=\"white-space: nowrap\">"+text+"</span></p>"}
 		.join("")
-	"<h1><a href=\"/servers/\">servers</a> / <a href=\"/server/?serverid=#{channel.server.id.to_s}\">#{channel.server.name}</a> / "+
-		"#{channel.name}</h1>#{(channel.topic.nil?)? "" : "<p>#{channel.topic}</p>"}"+
-	'<form method="post" action="/post/"><input type="hidden" name="channelid" value="'+id.to_s+'">'+
-		'<input type="text" name="text"><input type="submit"/></form>'+
-	"<div>"+timeline+"</div>"
+	body_part(channel.name,
+		"<h1><a href=\"/servers/\">servers</a> &gt; <a href=\"/server/?serverid=#{server.id.to_s}\">#{server.name}</a> &gt; "+
+			"#{channel.name}</h1>#{(channel.topic.nil?)? "" : "<p>#{channel.topic}</p>"}"+
+		'<form method="post" action="/post/"><input type="hidden" name="channelid" value="'+id.to_s+'">'+
+			'<input type="text" name="text"><input type="submit"/></form>'+
+		"<div>"+timeline+"</div>")
 end
 
 post "/post/" do
